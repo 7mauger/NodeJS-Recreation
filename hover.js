8fc2792889f8e42641e919f05862e2a95fc57217
7mauger <7mauger@stpatrick.org>
2017-04-23 12:57:03 -0500

    Add files via upload

--- hover.js
+++ hover.js
@@ -1,22 +1,7 @@
-let doc = document;
-let state = false;
-let element = doc.getElementsByClass('div_buttons1');
-function hover(a,b){
-    state = true;
-    if(a = 1){
-        hoverOn(a);
-    }
-    else{
-        hoverOn(b);
-    }
+function hoverOn(element){
+    element.style.opacity="0.8";
 }
 
-function hoverOn(x){
-    if(state === true){
-        element.style.opacity="0.5";
-        state = false;
-    }
-    else{
-        doc.getElementsByClass('div_buttons').style.opacity="1";
-    }
+function hoverOff(element){
+    element.style.opacity="1";
 }
